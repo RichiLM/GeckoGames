2f3dd90a115b6df4c3e23d9ee07fe97485cc9a89
RichiLM <richar12.rlom@gmail.com>
2024-03-14 11:30:32 -0600

    corregi etiquetas

--- components/nosotros.php
+++ components/nosotros.php
@@ -29,38 +29,35 @@ $ruta = '../';
         <img src="../assets/Poster/somos.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
-    <div class="container">
     <div class="row">
-    &nbsp;
-    <div class="container-fluid">
+      <div class="col-sm">
+        &nbsp;
+      </div>
+    </div>
     <div class="row text-center mx-3 mt-5 mb-5">
       <div class="col-sm">
         <h2 class=" display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">Somos una marca que ofrece perifericos gamer de un alto desempeño, diseño original y sobre todo la mas alta calidad para que puedas sacar tu maximo potencial en cada partida</h2>
       </div>
     </div>
-  </div>
-  <div class="container-fluid">
     <div class="row">
       <div class="col-sm">
         <img src="../assets/Poster/Poster2.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
-  </div>
-    
-  </div>
-  <div class="row">
-    &nbsp;
-    <div class="container-fluid">
-    <div class="row mx-3 mt-5 mb-5">
-        <div class="col-sm-6"> 
-            <h2 class="display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">En Gecko Gaming nos preocupamos por cada detalle, así que mires por donde mires nuestros productos, siempre contarán con los mayores cuidados en cada parte</h2>
-        </div>
-        <div class="col-sm-5"> 
-            <img src="../assets/Mouse/M8.png" width="500" alt="">
-        </div>
+    <div class="row">
+      <div class="col-sm">
+        &nbsp;
+      </div>
     </div>
-</div>
-
+    <div class="row mx-3 mt-5 mb-5">
+      <div class="col-sm-6">
+        <h2 class="display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">En Gecko Gaming nos preocupamos por cada detalle, así que mires por donde mires nuestros productos, siempre contarán con los mayores cuidados en cada parte</h2>
+      </div>
+      <div class="col-sm-5">
+        <img src="../assets/Mouse/M8.png" width="500" alt="">
+      </div>
+    </div>
+  </div>
 
   <?php
   require($ruta . 'layouts/footer.php');
