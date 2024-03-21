36a790b6f9786487017c5cf2ec504527d42d537a
BR105 <brbruno1055@gmail.com>
2024-03-20 19:09:04 -0600

    Nosotros terminado

--- components/nosotros.php
+++ components/nosotros.php
@@ -49,14 +49,43 @@ $ruta = '../';
         &nbsp;
       </div>
     </div>
-    <div class="row mx-3 mt-5 mb-5">
-      <div class="col-sm-6">
-        <h2 class="display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">En Gecko Gaming nos preocupamos por cada detalle, así que mires por donde mires nuestros productos, siempre contarán con los mayores cuidados en cada parte</h2>
-      </div>
-      <div class="col-sm-5">
-        <img src="../assets/Mouse/M8.png" width="500" alt="">
-      </div>
-    </div>
+   <div class="row mx-3 mt-5 mb-5 align-items-center">
+  <div class="col-sm-6 mb-4">
+    <h2 class="display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">En Gecko Gaming nos preocupamos por cada detalle, así que mires por donde mires nuestros productos, siempre contarán con los mejores acabados</h2>
+  </div>
+  <div class="col-sm-5">
+    <img src="../assets/Mouse/M8.png" width="500" alt="">
+  </div>
+</div>
+<div class="row mx-3 mt-5 mb-5 align-items-center">
+ <div class="col-sm-6 mb-4">
+<img src="../assets/Teclados/Switch.png" width="500" alt="">
+  </div>
+  <div class="col-sm-6 mb-4">
+    <h2 class="display-1 text-uppercase my-auto" style="color: white; font-size: 28px;">Pero sabemos que no solo importa lo de afuera y por eso cada uno de nuestros productos cuenta con con los mejores componentes </h2>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm text-center">
+    <h2 class="titulo display-1 text-uppercase my-auto mt-4 mb-4">ES POR ESO QUE</h2>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm text-center">
+    <img src="../assets/WP/P4.gif" class="d-block w-100 mt-4 mb-4" alt="...">
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm text-center">
+    <h2 class="titulo display-1 text-uppercase my-auto mt-4 mb-4">ES TÚ MEJOR OPCIÓN</h2>
+  </div>
+</div>
+
+
+
+
   </div>
 
   <?php
